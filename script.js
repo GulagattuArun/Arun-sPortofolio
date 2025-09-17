511e0cce2de0eec18a3e0ceccc3a929daa68d0a0
GULAGATTU ARUN <arungulagattu012@gmail.com>
2025-09-17 15:13:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let page=document.getElementById('next_page');
+function main_page(){
+    window.location.href='index2.html';
+}
+function mouse_over(){
+    page.style.backgroundColor='White';
+}
+function mouse_out(){
+    page.style.backgroundColor='initial';
+}
+function back_home(){
+    window.location.href='index2.html';
+}
+function project_page(){
+    window.location.href='projects.html'
+}
+function skill_page(){
+    window.location.href='skills.html';
+}
+function back(){
+    window.location.href='index.html';
+}
+function contact_page(){
+    window.location.href='contact.html';
+}
